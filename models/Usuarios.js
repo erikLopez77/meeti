9a1749374e98614c0c00801aa105519a89620868
ErikLopez <espinozalopezerik@gmail.com>
2025-05-24 23:48:42 -0600

    falta db

---
+++ models/Usuarios.js
@@ -0,0 +1,51 @@
+const Sequelize = require('sequelize');
+const db = require('../config/db');
+const bcrypt = require('bcrypt-nodejs');
+const { Hooks } = require('sequelize/lib/hooks');
+
+const Usuarios = db.define('usuarios', {
+    id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    }, nombre: Sequelize.STRING(60),
+    imagen: Sequelize.STRING(60),
+    email: {
+        type: Sequelize.STRING(30),
+        allowNull: false,
+        validate: {
+            isEmail: { msg: 'Agrega un correo válido' }
+        },
+        unique: {
+            args: true,
+            msg: 'Usuario ya registrado'
+        }, password: {
+            type: Sequelize.STRING(60),
+            allowNull: false,
+            validate: {
+                notEmpty: {
+                    msg: 'El password no puede ir vacio'
+                }
+            }
+        }, activo: {
+            type: Sequelize.INTEGER(1),
+            defaultValue: 0
+        },
+        tokenPassword: Sequelize.STRING,
+        expiraTokem: Sequelize.DATE
+    }
+}, {
+    hooks: {
+        beforeCreate(usuario) {
+            usuario.password = bcrypt.hashSync(usuario.password, bcrypt.genSaltSync(10), null)
+        }
+    }
+});
+
+//metodo para comparar los password
+Usuarios.prototype.validarPasswordl = function (password) {
+    return bcrypt.compareSync(password, this.password);
+}
+module.exports = Usuarios;
+
+
