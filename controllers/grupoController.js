509a5755c40e4de74509f149a2640be85971cee2
ErikLopez <espinozalopezerik@gmail.com>
2025-06-08 21:46:22 -0600

    validacion en las imagemes

--- controllers/grupoController.js
+++ controllers/grupoController.js
@@ -1,60 +1,78 @@
 const Categorias = require('../models/Categoria');
 const Grupos = require('../models/Grupos');
-const {check} = require('express-validator');
+const { check } = require('express-validator');
 const multer = require('multer');//p subir imagenes
-const shortid=require('shortid');//para id
+const shortid = require('shortid');//para id
 
-const configuracionMulter={
-    storage:fileStorage=multer.diskStorage({
-        destination:(req,file,next)=>{
-            next(null,__dirname+'/../public/uploads/grupos');
+const configuracionMulter = {
+    limits: { fileSize: 100000 },
+    storage: fileStorage = multer.diskStorage({
+        destination: (req, file, next) => {
+            next(null, __dirname + '/../public/uploads/grupos');
         },
-        filename:(req,file,next)=>{
-            const extension=file.mimetype.split('/')[1];
-            next(null,`${shortid.generate()}.${extension}`)
+        filename: (req, file, next) => {
+            const extension = file.mimetype.split('/')[1];
+            next(null, `${shortid.generate()}.${extension}`)
         }
-    })
+    }), fileFilter(req, file, next) {
+        if (file.mimetype == 'image/jpeg' || file.mimetype == 'image/png') {
+            next(null, true);
+        } else {
+            next(new Error('Formato no válido'), false);
+        }
+    }
 }
 
-const upload= multer(configuracionMulter).single('imagen');
+const upload = multer(configuracionMulter).single('imagen');
 //subir imagen en el server
-exports.subirImagen=(req,res,next)=>{
-    upload(req,res,function(error){
-        if(error){
-            console.log(error);
+exports.subirImagen = (req, res, next) => {
+    upload(req, res, function (error) {
+        if (error) {
+            if (error instanceof multer.MulterError) {
+                if (error.code == 'LIMIT_FILE_SIZE') {
+                    req.flash('error', 'El archivo es muy grande');
+                } else {
+                    req.flash('error', error.message)
+                }
+            } else if (error.hasOwnProperty('message')) {
+                req.flash('error', error.message);
+            }
+            res.redirect('back');
+            return;
             //TODO: Manejar errores
-        }else{
+        } else {
             next();
         }
     })
 }
 
-exports.formNuevoGrupo = async (req,res) =>{
+exports.formNuevoGrupo = async (req, res) => {
     const categorias = await Categorias.findAll();
-    res.render('nuevo-grupo',{
-        nombrePagina:'Crea un nuevo grupo',
+    res.render('nuevo-grupo', {
+        nombrePagina: 'Crea un nuevo grupo',
         categorias
     })
 }
 //alamcena grupos en la db
-exports.crearGrupo = async(req,res)=>{
+exports.crearGrupo = async (req, res) => {
     await check('nombre').escape().run(req);
     await check('url').escape().run(req);
     const grupo = req.body;
     //almacena el usuario autenticado como el creador del grupo
-    grupo.usuarioId=req.user.id;
-
-    grupo.imagen=req.file.filename;
+    grupo.usuarioId = req.user.id;
+    if (req.file) {
+        grupo.imagen = req.file.filename;
+    }
 
     try {
         await Grupos.create(grupo);
 
-        req.flash('exito','Se ha creado el grupo correctamente');
+        req.flash('exito', 'Se ha creado el grupo correctamente');
         res.redirect('/administracion');
     } catch (error) {
         console.log(error);
         const erroresSequelize = error.errors ? error.errors.map(err => err.message) : [];
-        req.flash('error',erroresSequelize);
+        req.flash('error', erroresSequelize);
         res.redirect('/nuevo-grupo');
     }
 }
