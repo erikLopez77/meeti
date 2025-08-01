d9bb908e377953bfe63723b54e748d912e963546
ErikLopez <espinozalopezerik@gmail.com>
2025-07-31 18:59:39 -0600

    ya cambia el mapa al escribir, faltan ajustes, se instalo geoserch

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,53 @@
-document.addEventListener('DOMContentLoaded', function () {
-    alert("holaa");
+import { OpenStreetMapProvider } from 'leaflet-geosearch';
 
-    var map = L.map('mapa').setView([51.505, -0.09], 13);
+const lat = 19.0403035;
+const lng = -98.1816911;
+const map = L.map('mapa').setView([lat, lng], 13);
+let markers = new L.FeatureGroup().addTo(map);
+let marker;
+
+document.addEventListener('DOMContentLoaded', function () {
 
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     }).addTo(map);
 
-    L.marker([51.5, -0.09]).addTo(map)
-        .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-        .openPopup();
+    const buscador = document.querySelector('#formbuscador');
+    buscador.addEventListener('input', buscarDireccion);
 });
+
+function buscarDireccion(e) {
+    if (e.target.value.length > 8) {
+        //si existe un pin anterior limpiarlo
+        markers.clearLayers();
+
+        //usar provider
+        const geocodeService = L.esri.Geocoding.geocodeService();
+        const provider = new OpenStreetMapProvider();
+        provider.search({ query: e.target.value }).then((resultado) => {
+            geocodeService.reverse().latlng(resultado[0].bounds[0], 15).run(function (error, result) {
+                llenarInputs(result);
+
+                //mostrar el mapa
+                map.setView(resultado[0].bounds[0], 15);
+                //agregar el pin
+                marker = new L.marker(resultado[0].bounds[0], {
+                    draggable: true,
+                    autoPan: true
+                })
+                    .addTo(map)
+                    .bindPopup(resultado[0].label)
+                    .openPopup();
+
+                //detectar e
+                marker.on('moveend', function (e) {
+                    marker = e.target;
+                    console.log(marker);
+                })
+
+            });
+
+
+        })
+    }
+}
